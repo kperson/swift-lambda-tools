10a31da09c0b4b1561b9672ff0880706b3b13a5b
Kelton Person <keltonperson@Keltons-MacBook-Pro.local>
2019-07-27 21:12:31 -0500

    update swift-aws

--- Sample/Sources/Util.swift
+++ Sample/Sources/Util.swift
@@ -11,40 +11,6 @@ import SNS
 import NIO
 import SwiftAWS
 import DynamoDB
-import Vapor
-
-public extension JSONEncoder {
-    
-    func asString<T: Encodable>(item: T) throws -> String {
-        let data = try encode(item)
-        return String(data: data, encoding: .utf8)!
-    }
-    
-    func asData<T: Encodable>(item: T) throws -> Data {
-            return try encode(item)
-    }
-    
-}
-
-public extension Encodable {
-    
-    func asJSONData(encoder: JSONEncoder = JSONEncoder()) throws -> Data {
-        return try encoder.asData(item: self)
-    }
-    
-    func asJSONString(encoder: JSONEncoder = JSONEncoder()) throws -> String {
-        return try encoder.asString(item: self)
-    }
-    
-}
-
-public extension JSONDecoder {
-    
-    func fromJSON<D: Decodable>(type: D.Type, str: String) throws -> D {
-        return try decode(type, from: str.data(using: .utf8) ?? "")
-    }
-    
-}
 
 
 public extension SQS {

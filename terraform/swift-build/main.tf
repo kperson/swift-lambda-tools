8577fdabc1e4fef45a5d1f859a8ee01565e3f478
Kelton Person <kelton.person@gmail.com>
2019-11-02 17:29:12 -0500

    updated more terraform

--- terraform/swift-build/main.tf
+++ terraform/swift-build/main.tf
@@ -27,17 +27,18 @@ resource "random_string" "tag" {
 }
 
 data "template_file" "build_script" {
-  template = "${file("${path.module}/image_build_push_script.tpl")}"
+  template = file(format("%s/image_build_push_script.tpl", path.module))
 
   vars = {
     tag         = random_string.tag.result
-    docker_file = "${var.docker_file == "NA" ? "Build/${path.module}/Dockerfile" : var.docker_file}"
+    docker_file = var.docker_file == "NA" ? format("Build/%s/Dockerfile", path.module) : var.docker_file
+    
   }
 }
 
 resource "null_resource" "docker_build" {
   triggers = {
-    time = "${timestamp()}"
+    time = timestamp()
   }
 
   provisioner "local-exec" {
@@ -51,10 +52,9 @@ resource "null_resource" "docker_build" {
 
 data "template_file" "docker_tag" {
   depends_on = ["null_resource.docker_build"]
-  template   = "$${output_file}"
+  template   = random_string.tag.result
 
   vars = {
-    output_file = random_string.tag.result
   }
 }
 
@@ -64,15 +64,15 @@ output "docker_tag" {
 
 
 data "template_file" "extract_script" {
-  template = "${file("${path.module}/extract_script.tpl")}"
-
+  template = file(format("%s/extract_script.tpl", path.module))
+  
   vars = {
     dind_mount     = var.dind_mount
     container_file = var.executable_location
     tag            = random_string.tag.result
     output_file    = "swiftApp"
     working_dir    = path.cwd
-    bootstrap_file = "${path.module}/bootstrap"
+    bootstrap_file = format("%s/bootstrap", path.module)
   }
 }
 
@@ -80,7 +80,7 @@ resource "null_resource" "docker_extract" {
   depends_on = ["null_resource.docker_build"]
 
   triggers = {
-    time = "${timestamp()}"
+    time = timestamp()
   }
 
   provisioner "local-exec" {
@@ -95,11 +95,11 @@ data "archive_file" "zip" {
   depends_on  = ["null_resource.docker_extract"]
   type        = "zip"
   source_dir  = random_string.tag.result
-  output_path = "${random_string.tag.result}.zip"
+  output_path = format("%s.zip", random_string.tag.result)
 }
 
 output "zip_file" {
-  value = "${random_string.tag.result}.zip"
+  value = format("%s.zip", random_string.tag.result)
 }
 
 output "zip_file_hash" {
@@ -112,8 +112,7 @@ resource "null_resource" "cleanup_dir" {
     time = timestamp()
   }
   provisioner "local-exec" {
-    command = "rm -rf ${random_string.tag.result}"
-
+    command = format("rm -rf %s", random_string.tag.result)
     environment = {
       ZIP_FILE_HASH = data.archive_file.zip.output_base64sha256
     }

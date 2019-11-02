efcb470952b7749f289c5eae0867be2ce573abc1
Kelton Person <kelton.person@gmail.com>
2019-11-02 15:57:29 -0500

    updated more modules

--- terraform/dynamo-stream-lambda/main.tf
+++ terraform/dynamo-stream-lambda/main.tf
@@ -4,6 +4,11 @@ variable "build_params" {
   type = "map"
 }
 
+variable "runtime_layers" {
+  type = "list"
+  default = []
+}
+
 variable "env" {
   type    = "map"
   default = {}
@@ -47,21 +52,21 @@ variable "stream_arn" {
 
 
 resource "aws_lambda_function" "lambda" {
-  filename         = "${var.build_params["zip_file"]}"
-  function_name    = "${var.function_name}"
-  role             = "${var.build_params["role"]}"
-  handler          = "${var.handler}"
+  filename         = var.build_params["zip_file"]
+  function_name    = var.function_name
+  role             = var.build_params["role"]
+  handler          = var.handler
   runtime          = "provided"
-  memory_size      = "${var.memory_size}"
-  timeout          = "${var.timeout}"
+  memory_size      = var.memory_size
+  timeout          = var.timeout
   publish          = true
-  layers           = ["${var.build_params["runtime_layer"]}"]
-  source_code_hash = "${var.build_params["zip_file_hash"]}"
+  layers           = var.runtime_layers
+  source_code_hash = var.build_params["zip_file_hash"]
 
 
   vpc_config {
-    subnet_ids         = "${var.subnet_ids}"
-    security_group_ids = "${var.security_group_ids}"
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
   }
 
   environment {
@@ -71,8 +76,8 @@ resource "aws_lambda_function" "lambda" {
 
 resource "aws_lambda_event_source_mapping" "lambda" {
   batch_size        = 100
-  event_source_arn  = "${var.stream_arn}"
+  event_source_arn  = var.stream_arn
   enabled           = true
-  function_name     = "${aws_lambda_function.lambda.arn}"
+  function_name     = aws_lambda_function.lambda.arn
   starting_position = "LATEST"
 }
--- terraform/swift-lambda/main.tf
+++ terraform/swift-lambda/main.tf
@@ -46,27 +46,27 @@ variable "timeout" {
 
 
 resource "aws_lambda_function" "lambda" {
-  filename         = "${var.build_params["zip_file"]}"
-  function_name    = "${var.function_name}"
-  role             = "${var.build_params["role"]}"
-  handler          = "${var.handler}"
+  filename         = var.build_params["zip_file"]
+  function_name    = var.function_name
+  role             = var.build_params["role"]
+  handler          = var.handler
   runtime          = "provided"
-  memory_size      = "${var.memory_size}"
-  timeout          = "${var.timeout}"
+  memory_size      = var.memory_size
+  timeout          = var.timeout
   publish          = true
-  layers           = "${var.runtime_layers}"
-  source_code_hash = "${var.build_params["zip_file_hash"]}"
+  layers           = var.runtime_layers
+  source_code_hash = var.build_params["zip_file_hash"]
 
   vpc_config {
-    subnet_ids         = "${var.subnet_ids}"
-    security_group_ids = "${var.security_group_ids}"
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
   }
 
   environment {
-    variables = "${var.env}"
+    variables = var.env
   }
 }
 
 output "arn" {
-  value = "${aws_lambda_function.lambda.arn}"
+  value = aws_lambda_function.lambda.arn
 }

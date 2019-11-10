de366bff3250ddd350b35473e720456e66b03d41
Kelton Person <kelton.person@gmail.com>
2019-11-10 14:25:13 -0600

    fixed output

--- terraform/swift-build/main.tf
+++ terraform/swift-build/main.tf
@@ -26,15 +26,6 @@ resource "random_string" "tag" {
   }
 }
 
-data "template_file" "build_script" {
-  template = file(format("%s/image_build_push_script.tpl", path.module))
-
-  vars = {
-    tag         = random_string.tag.result
-    docker_file = var.docker_file == "NA" ? format("Build/%s/Dockerfile", path.module) : var.docker_file
-    
-  }
-}
 
 resource "null_resource" "docker_build" {
   triggers = {
@@ -43,48 +34,37 @@ resource "null_resource" "docker_build" {
 
   provisioner "local-exec" {
     working_dir = var.working_dir
-    command     = data.template_file.build_script.rendered
+    command = templatefile(format("%s/image_build_push_script.tpl", path.module), {
+      tag         = random_string.tag.result,
+      docker_file = var.docker_file == "NA" ? format("Build/%s/Dockerfile", path.module) : var.docker_file
+    })
 
     environment = {
     }
   }
 }
 
-data "template_file" "docker_tag" {
-  depends_on = ["null_resource.docker_build"]
-  template   = random_string.tag.result
-
-  vars = {
-  }
-}
-
 output "docker_tag" {
-  value = data.template_file.docker_tag.rendered
-}
-
-
-data "template_file" "extract_script" {
-  template = file(format("%s/extract_script.tpl", path.module))
-  
-  vars = {
-    dind_mount     = var.dind_mount
-    container_file = var.executable_location
-    tag            = random_string.tag.result
-    output_file    = "swiftApp"
-    working_dir    = path.cwd
-    bootstrap_file = format("%s/bootstrap", path.module)
-  }
+  depends_on = [null_resource.docker_build]
+  value = random_string.tag.result
 }
 
 resource "null_resource" "docker_extract" {
-  depends_on = ["null_resource.docker_build"]
+  depends_on = [null_resource.docker_build]
 
   triggers = {
     time = timestamp()
   }
 
   provisioner "local-exec" {
-    command = data.template_file.extract_script.rendered
+    command = templatefile(file(format("%s/extract_script.tpl", path.module)), {
+      dind_mount     = var.dind_mount,
+      container_file = var.executable_location,
+      tag            = random_string.tag.result,
+      output_file    = "swiftApp",
+      working_dir    = path.cwd,
+      bootstrap_file = format("%s/bootstrap", path.module)
+    })
 
     environment = {
     }
@@ -92,7 +72,7 @@ resource "null_resource" "docker_extract" {
 }
 
 data "archive_file" "zip" {
-  depends_on  = ["null_resource.docker_extract"]
+  depends_on  = [null_resource.docker_extract]
   type        = "zip"
   source_dir  = random_string.tag.result
   output_path = format("%s.zip", random_string.tag.result)
@@ -117,5 +97,5 @@ resource "null_resource" "cleanup_dir" {
       ZIP_FILE_HASH = data.archive_file.zip.output_base64sha256
     }
   }
-  
+
 }

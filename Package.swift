156184be7cb0e52248a574e2a8e3228b343b6d78
Kelton Person <kelton.person@gmail.com>
2020-04-21 19:07:06 -0500

    reverted to master branch

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "SwiftAWS", targets: ["SwiftAWS"])
     ],
     dependencies: [
-        .package(url: "https://github.com/kperson/vapor-lambda-adapter.git", .revision("f9c50553bb0a90829a3d073a78e73a6ad44fcab0"))
+        .package(url: "https://github.com/kperson/vapor-lambda-adapter.git", .revision("master"))
     ],
     targets: [
         .target(

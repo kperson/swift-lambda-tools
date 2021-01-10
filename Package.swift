2f882a72b923e4ef5cdba58413721dfc562f5471
Kelton Person <kelton.person@gmail.com>
2021-01-10 14:01:04 -0600

    added updated logger

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "SwiftAWS", targets: ["SwiftAWS"])
     ],
     dependencies: [
-        .package(url: "https://github.com/kperson/vapor-lambda-adapter.git", .branch("master"))
+        .package(url: "https://github.com/kperson/vapor-lambda-adapter.git", .upToNextMinor(from: "1.0.0"))
     ],
     targets: [
         .target(
